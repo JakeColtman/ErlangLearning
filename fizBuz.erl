ab3ec9487ea812929f360eb1bd147dace25e9015
Jake Coltman <jakecoltman@sky.com>
2015-11-01 21:17:04 +0000

    Added a max limit

--- fizBuz.erl
+++ fizBuz.erl
@@ -1,22 +1,22 @@
 -module(fizbuz).
--export([step/1]).
+-export([step/2]).
 -author("Journeyman08").
 
-step(10) -> 
+step(X, Max) when X >= Max -> 
   io:format("GAMEOVER");
   
-step(X) when X rem 15 =:= 0 ->
+step(X, Max) when X rem 15 =:= 0 ->
   io:format("FizBuz ~n"),
-  step(X + 1);
+  step(X + 1, Max);
   
-step(X) when X rem 5 =:= 0 ->
+step(X, Max) when X rem 5 =:= 0 ->
   io:format("Buz ~n"),
-  step(X + 1);
+  step(X + 1, Max);
   
-step(X) when X rem 3 =:= 0 ->
+step(X, Max) when X rem 3 =:= 0 ->
   io:format("Fiz ~n"),
-  step(X + 1);
+  step(X + 1, Max);
   
-step(X) ->
-  io:format("~w", [X]),
-  step(X + 1).
+step(X, Max) ->
+  io:format("~w ~n", [X]),
+  step(X + 1, Max).
