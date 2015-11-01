8c9d1234511e47258f6723e258a225678fdda651
Jake Coltman <jakecoltman@sky.com>
2015-11-01 21:13:58 +0000

    Rewritten to use guards

--- fizBuz.erl
+++ fizBuz.erl
@@ -5,11 +5,18 @@
 step(10) -> 
   io:format("GAMEOVER");
   
-step(5) -> 
-  io:format("Buz");
+step(X) when X rem 15 =:= 0 ->
+  io:format("FizBuz ~n"),
+  step(X + 1);
   
-step(3) -> 
-  io:format("Fiz");
+step(X) when X rem 5 =:= 0 ->
+  io:format("Buz ~n"),
+  step(X + 1);
+  
+step(X) when X rem 3 =:= 0 ->
+  io:format("Fiz ~n"),
+  step(X + 1);
   
 step(X) ->
-  io:format("~w", [X]).
+  io:format("~w", [X]),
+  step(X + 1).
